b54cc41afaf2114a5783ac413ddc050819a4140e
Nick Cesare <ncesare@gmail.com>
2022-10-04 21:07:16 -0600

    Random AI working

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const squares = document.querySelectorAll('.container div');
 const winnerDisplay = document.querySelector('.winner');
 
 const protoPlayer = function(isHuman, marker) {
-    if (isHuman === false) console.log('ai stuff');
     let isTurn = false;
     if (marker === 'X') isTurn = true;
 
-    return {marker, isTurn};
+    return {marker, isTurn, isHuman};
 }
 
 const player1 = protoPlayer(true, 'X');
-const player2 = protoPlayer(true, 'O');
+const player2 = protoPlayer(false, 'O');
 
 const players = [player1, player2];
 let currentPlayer = players.find(player => player.isTurn === true);
@@ -41,8 +40,7 @@ const gameBoard = (() => {
             if (square.status === '') continue;
             else if (square.x === 0 && checkRow() || 
                     square.y === 0 && checkColumn() || 
-                    square.x === 0 && square.y === 0 && checkDiagonal() ||
-                    square.x === 2 && square.y === 0 && checkDiagonal()) {
+                    checkDiagonal()) {
                         gameOn = false;
                         return winnerDisplay.textContent = `${square.status} wins`;
                     }
@@ -66,10 +64,14 @@ const gameBoard = (() => {
             const square2 = squareArray.find(element => element.x === 1 && element.y === 1);
             let square3 = squareArray.find(element => element.x === 2 && element.y === 2);
 
-            if (square.status === square2.status && square.status === square3.status) return true;
+            if (square.x === 0 && square.y === 0 && 
+                square.status === square2.status && 
+                square.status === square3.status) return true;
 
             square3 = squareArray.find(element => element.x === 0 && element.y === 2);
-            if (square.status === square2.status && square.status === square3.status) return true;
+            if (square.x === 2 && square.y === 0 && 
+                square.status === square2.status && 
+                square.status === square3.status) return true;
         }
     }
 
@@ -77,6 +79,15 @@ const gameBoard = (() => {
         if (currentPlayer.marker === 'X') currentPlayer = players.find(player => player.marker === 'O');
         else currentPlayer = players.find(player => player.marker === 'X');
     }
+
+    const gameAI = function() {
+        let computerPlayer = players.find(player => !player.isHuman)
+        const randInt = Math.floor(Math.random() * 9);
+        const selection = squares[randInt];
+        const selectionObject = squareArray[randInt];
+        if (selection.textContent === '') return selection.textContent = selectionObject.status = selectionObject.updateStatus();
+        else return gameAI();
+    }    
 
     squares.forEach(square => {
         const squareObject = createSquare(square, x, y);
@@ -91,8 +102,12 @@ const gameBoard = (() => {
             if (gameOn) {
                 square.textContent = squareObject.status = squareObject.updateStatus();
                 changePlayers();
+                if (currentPlayer.isHuman === false) {
+                    gameAI();
+                    changePlayers();
+                }
                 checkWin();
             }
         }, {once: true});
-});
+    });
 })();
