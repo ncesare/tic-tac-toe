e991ea6276496c1512f1de98c4e2472e53159020
Nick Cesare <ncesare@gmail.com>
2022-10-04 19:26:13 -0600

    Basic functionality complete

--- script.js
+++ script.js
@@ -1,42 +1,94 @@
 const squares = document.querySelectorAll('.container div');
+const winnerDisplay = document.querySelector('.winner');
 
-let squareArray = [];
+const protoPlayer = function(isHuman, marker) {
+    if (isHuman === false) console.log('ai stuff');
+    let isTurn = false;
+    if (marker === 'X') isTurn = true;
 
-// Each square is an object instance
-// Square object has methods: add X/O, report on position and status ({x: 0, y: 0, status: 'X'})
-// Board is an object
-// onclick Board checks for win conditions, has method to report board status in array of coordinates
-// If status at square$ === status at square$(x + 1 || y + 1 || x & y + 1) and for + 2, then {status} is winner
+    return {marker, isTurn};
+}
 
+const player1 = protoPlayer(true, 'X');
+const player2 = protoPlayer(true, 'O');
 
-const squareFactory = function(square, x, y) {
-    let status = square.textContent;
+const players = [player1, player2];
+let currentPlayer = players.find(player => player.isTurn === true);
+
+const createSquare = function(square, x, y) {
+    let status = '';
 
     const updateStatus =  function() {
         console.log(x, y)
-        if (square.textContent === '') return 'X';
+        if (square.textContent === '') return currentPlayer.marker;
         else return '';
     }
 
     return {x, y, status, updateStatus};
 }
 
-// Can I make this part of a gameboard object, initialized as a module?
+const gameBoard = (() => {
+    let gameOn = true;
 
-let x = 0;
-let y = 0;
+    let squareArray = [];
 
-squares.forEach(square => {
-    const squareObject = squareFactory(square, x, y);
-    squareArray.push(squareObject);
-    if (x < 2) x++
-    else {
-        x = 0;
-        y++;
+    let x = 0;
+    let y = 0;
+
+    const checkWin = function() {
+        // Iterate over squares on the board and check win conditions according to starting square and direction.
+
+        for (square of squareArray) {
+            if (square.status === '') continue;
+            else if (square.x === 0 && checkRow() || 
+                    square.y === 0 && checkColumn() || 
+                    square.x === 0 && square.y === 0 && checkDiagonal()) {
+                        gameOn = false;
+                        return winnerDisplay.textContent = `${square.status} wins`;
+                    }
+        }
+
+        function checkRow() {
+            const square2 = squareArray.find(element => element.x === square.x + 1 && element.y === square.y);
+            const square3 = squareArray.find(element => element.x === square.x + 2 && element.y === square.y);
+
+            if (square.status === square2.status && square.status === square3.status) return true;
+        }
+
+        function checkColumn() {
+            const square2 = squareArray.find(element => element.y === square.y + 1 && element.x === square.x);
+            const square3 = squareArray.find(element => element.y === square.y + 2 && element.x === square.x);
+
+            if (square.status === square2.status && square.status === square3.status) return true;
+        }
+
+        function checkDiagonal() {
+            const square2 = squareArray.find(element => element.x === 1 && element.y === 1);
+            const square3 = squareArray.find(element => element.x === 2 && element.y === 2);
+
+            if (square.status === square2.status && square.status === square3.status) return true;
+        }
     }
 
-    square.addEventListener('click', () => {
-        square.textContent = squareObject.updateStatus();
-        console.log(squareObject.status);
-    })
+    const changePlayers = function() {
+        if (currentPlayer.marker === 'X') currentPlayer = players.find(player => player.marker === 'O');
+        else currentPlayer = players.find(player => player.marker === 'X');
+    }
+
+    squares.forEach(square => {
+        const squareObject = createSquare(square, x, y);
+        squareArray.push(squareObject);
+        if (x < 2) x++
+        else {
+            x = 0;
+            y++;
+        }
+
+        square.addEventListener('click', () => {
+            if (gameOn) {
+                square.textContent = squareObject.status = squareObject.updateStatus();
+                changePlayers();
+            }
+        });
 });
+})();
